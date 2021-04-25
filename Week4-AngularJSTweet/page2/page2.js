85a3b3e082428a8cc984bc764e39d00eea9f6534
Raulito <ralvaradojr399@gmail.com>
2021-04-25 16:27:58 -0400

    refactored Tweeter project

---
+++ Week4-AngularJSTweet/page2/page2.js
@@ -0,0 +1,32 @@
+angular.module("myApp.page2",['ngRoute','firebase'])
+.config(['$routeProvider',function($routeProvider){
+	$routeProvider
+		.when('/page2',{
+			templateUrl:"page2/page2.html",
+			controller:"page2Ctrl"
+		});
+}])
+.controller('page2Ctrl', function($scope,$firebaseArray){
+	$scope.addTweet = function(){
+		$scope.msg2="";
+		var ref = firebase.database().ref("tweeter");
+		$firebaseArray(ref).$add($scope.tweeter)
+		.then(
+			function(ref){
+				$scope.tweeter.name = "";
+        $scope.tweeter.date = "";
+				$scope.tweeter.text = "";
+				$scope.msg2= "Tweet added successfully.";
+				window.setTimeout(function(){
+					$scope.$apply(function(){
+						$scope.msg2 = false;
+					})
+				},2000)
+			},
+			function(error){
+				console.log(error);
+			}
+			)
+	};
+	console.log('page2');
+});
---
+++ Week4-AngularJSTweet/page3/page3.js
@@ -0,0 +1,47 @@
+angular.module("myApp.page3",['ngRoute','firebase'])
+.config(['$routeProvider',function($routeProvider){
+	$routeProvider
+		.when('/page3/:id',{
+			templateUrl:"page3/page3.html",
+			controller:"page3Ctrl"
+		});
+}])
+.controller('page3Ctrl', function($scope,$firebaseArray,$firebaseObject,$routeParams){
+	$scope.msg3="";
+	var id = $routeParams.id;
+	var ref = firebase.database().ref("tweeter/"+id);
+	$scope.tweeter = $firebaseObject(ref);
+
+	$scope.editTweet = function(id){
+		console.log(id);
+
+		var ref = firebase.database().ref("tweeter/"+id);
+		ref.update({
+			name:$scope.tweeter.name,
+			department:$scope.tweeter.datetime,
+			level:$scope.tweeter.text,
+		})
+		.then(
+			function(ref){
+				$scope.tweeter.name= "";
+				$scope.tweeter.datetime="";
+				$scope.tweeter.text = "";
+				$scope.msg3= "updated successfully.";
+				window.setTimeout(function(){
+					$scope.$apply(function(){
+						
+					})
+				},100);
+				window.setTimeout(function(){
+					$scope.$apply(function(){
+						$scope.msg3 = false;
+					})
+				},2000)			}
+			,function(error){
+				console.log(error);
+			}
+		);
+	}
+
+	
+});
